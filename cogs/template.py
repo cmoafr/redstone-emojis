f427d035d91d71c3087c51e49d5794db4902ed4e
cmoafr <cmoa58@gmail.com>
2022-05-07 21:19:31 +0200

    Update template

--- cogs/template.py
+++ cogs/template.py
@@ -1,7 +1,10 @@
+from discord import app_commands
 from discord.ext import commands
 
 async def setup(bot):
-    await bot.add_cog(Template(bot))
+    # Do not register this cog as it is just for showcase.
+    # Remove the `pass #` on the following line to make it work.
+    pass #await bot.add_cog(Template(bot))
 
 class Template(commands.Cog):
     
@@ -10,9 +13,17 @@ class Template(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        pass
-        #self.bot.logger.info("Cog template ready!")
+        self.bot.logger.info("Cog template ready!")
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        pass
+        await message.channel.send("Message received!")
+
+    @app_commands.command(name="template", description="This is a template command")
+    @app_commands.describe(value="Description of the parameter")
+    @app_commands.rename(value="custom_display_name")
+    async def template(self, interaction, value: str = "Default value"):
+        await interaction.response.send_message(value)
+
+# Examples of commands:
+# https://github.com/Rapptz/discord.py/blob/master/examples/app_commands/basic.py
