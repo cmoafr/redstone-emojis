81483809412dd51d085df62154208ae78c38bbbb
cmoafr <cmoa58@gmail.com>
2022-07-20 18:35:22 +0200

    Implemented Done button
    Added grid on every block
    Now passing the sizes as parameters

--- editor/base.py
+++ editor/base.py
@@ -8,8 +8,13 @@ import requests
 from utils.config import get_config
 
 BLOCK_SIZE = 64
-BORDER_SIZE = 8
+BORDER_SIZE = 1
+INVERT_SIZE = 8
+
+BACKGROUND_COLOR = (54, 57, 63, 0)
+BORDER_COLOR = (255, 255, 255, 128)
 RENDER_DISTANCE = 5
+
 NONE = "Air"
 DEFAULT = "Block"
 
@@ -23,29 +28,39 @@ class BaseView(discord.ui.View):
         super().__init__()
 
     @lru_cache(maxsize=64)
-    def get_emoji(self, block=None, size=16, invert_border=False, border_size=1):
+    def get_emoji(self, block=None, size=16):
         if block is None:
             block = self.block
-
-        # Get the image
         emoji = self.bot.get_emoji(block)
+
         if emoji is None:
-            image = Image.new("RGBA", (size, size), (54, 57, 63, 0))
             self.bot.logger.warning(f"No emoji found with id {block}")
-        else:
-            r = requests.get(str(emoji.url), stream=True)
-            image = Image.open(r.raw)
-        image = image.convert("RGBA").resize((size, size), resample=0)
+            return Image.new("RGBA", (size, size), BACKGROUND_COLOR)
+        
+        r = requests.get(str(emoji.url), stream=True)
+        return Image.open(r.raw).convert("RGBA").resize((size, size), resample=0)
+
+    #@lru_cache(maxsize=64)
+    def process_emoji(self, image, grid_color, grid_width=1, invert_border_size=0):
+
+        # Create border
+        if grid_width:
+            end = image.width - grid_width
+            bg = Image.new("RGBA", image.size, grid_color)
+            bg.paste(BACKGROUND_COLOR, (grid_width, grid_width, end, end))
+            bg.paste(image, mask=image)
+            image = bg
 
         # Invert the border
-        if invert_border:
-            cropped = image.crop((border_size, border_size, image.width - border_size, image.height - border_size))
+        if invert_border_size:
+            cropped = image.crop((invert_border_size, invert_border_size, image.width - invert_border_size, image.height - invert_border_size))
             image = ImageOps.invert(image.convert("RGB")).convert("RGBA")
-            image.paste(cropped, (border_size, border_size))
-        
+            image.paste(cropped, (invert_border_size, invert_border_size))
+
         return image
 
-    def get_image(self):
+    def get_image(self, render_distance=RENDER_DISTANCE, block_size=BLOCK_SIZE, border_size=BORDER_SIZE, invert_size=INVERT_SIZE, expand_cursor=True):
+
         if self.grid:
             # Get the grid bounds
             min_x = min(self.grid, key=lambda x: x[0])[0]
@@ -54,35 +69,43 @@ class BaseView(discord.ui.View):
             max_y = max(self.grid, key=lambda x: x[1])[1]
 
             # Expand to curor
-            min_x = min(min_x, self.x)
-            min_y = min(min_y, self.y)
-            max_x = max(max_x, self.x)
-            max_y = max(max_y, self.y)
+            if expand_cursor:
+                min_x = min(min_x, self.x)
+                min_y = min(min_y, self.y)
+                max_x = max(max_x, self.x)
+                max_y = max(max_y, self.y)
 
         else:
             min_x, min_y, max_x, max_y = self.x, self.y, self.x, self.y
 
         # Clamp to the render distance
-        min_x = max(min_x, self.x - RENDER_DISTANCE)
-        max_x = min(max_x, self.x + RENDER_DISTANCE) + 1
-        min_y = max(min_y, self.y - RENDER_DISTANCE)
-        max_y = min(max_y, self.y + RENDER_DISTANCE) + 1
+        min_x = max(min_x, self.x - render_distance)
+        max_x = min(max_x, self.x + render_distance) + 1
+        min_y = max(min_y, self.y - render_distance)
+        max_y = min(max_y, self.y + render_distance) + 1
 
         # Create the image
         image = Image.new(
-            "RGBA", (
-            BLOCK_SIZE * (max_x - min_x),
-            BLOCK_SIZE * (max_y - min_y)
-        ))
+            "RGBA",
+            (
+                block_size * (max_x - min_x),
+                block_size * (max_y - min_y)
+            ),
+            BACKGROUND_COLOR
+        )
+
         for x in range(min_x, max_x):
             for y in range(min_y, max_y):
+
                 block = self.grid.get((x, y), self.blocks[NONE])
                 invert = x == self.x and y == self.y
-                emoji = self.get_emoji(block, BLOCK_SIZE, invert, BORDER_SIZE)
+                emoji = self.get_emoji(block, block_size)
+                emoji = self.process_emoji(emoji, BORDER_COLOR, (not invert)*border_size, invert*invert_size)
+
                 image.paste(
                     emoji, (
-                    (x - min_x) * BLOCK_SIZE,
-                    (y - min_y) * BLOCK_SIZE
+                    (x - min_x) * block_size,
+                    (y - min_y) * block_size
                 ))
         
         return image
--- editor/main.py
+++ editor/main.py
@@ -1,15 +1,22 @@
 import discord
 
-from editor.base import BaseView
+from io import BytesIO
+
+from editor.base import BaseView, NONE
 from editor.search import Search
 
 class MainView(BaseView):
     def __init__(self, bot):
         super().__init__(bot)
+        self.update_buttons()
+
+    def update_buttons(self):
+        self.done.disabled = not self.grid
 
     @discord.ui.button(label='Undo', style=discord.ButtonStyle.red, row=0, disabled=True)
     async def undo(self, interaction, button):
         # TODO
+        self.update_buttons()
         await self.send(interaction)
 
     @discord.ui.button(emoji="\u2b06", style=discord.ButtonStyle.blurple, row=0)
@@ -20,6 +27,7 @@ class MainView(BaseView):
     @discord.ui.button(label='Redo', style=discord.ButtonStyle.green, row=0, disabled=True)
     async def redo(self, interaction, button):
         # TODO
+        self.update_buttons()
         await self.send(interaction)
 
     @discord.ui.button(emoji='\u2b05', style=discord.ButtonStyle.blurple, row=1)
@@ -29,7 +37,11 @@ class MainView(BaseView):
 
     @discord.ui.button(label='Place', style=discord.ButtonStyle.blurple, row=1)
     async def place(self, interaction, button):
-        self.grid[(self.x, self.y)] = self.blocks[self.block]
+        if self.block == NONE:
+            del self.grid[(self.x, self.y)]
+        else:
+            self.grid[(self.x, self.y)] = self.blocks[self.block]
+        self.update_buttons()
         await self.send(interaction)
 
     @discord.ui.button(emoji='\u27a1', style=discord.ButtonStyle.blurple, row=1)
@@ -48,5 +60,20 @@ class MainView(BaseView):
 
     @discord.ui.button(label='Done', style=discord.ButtonStyle.green, row=2, disabled=True)
     async def done(self, interaction, button):
-        # TODO
-        await self.send(interaction)
+        if not self.grid:
+            # Would generate an empty image, cancel
+            await interaction.response.defer()
+            return
+
+        image = self.get_image(
+            render_distance=float("inf"), # Get the full image
+            border_size=0,
+            invert_size=0,
+            expand_cursor=False
+        )
+        with BytesIO() as image_bin:
+            image.save(image_bin, "PNG")
+            image_bin.seek(0)
+            file = discord.File(image_bin, filename="circuit.png")
+
+        await interaction.response.edit_message(content=None, view=None, attachments=[file])
