1cc2e2bebbd3fdc4c4c1cd93158379d2d9bb301d
cmoafr <cmoa58@gmail.com>
2022-08-07 22:09:40 +0200

    Added conditions to autoreply

--- cogs/autoreply.py
+++ cogs/autoreply.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from datetime import datetime, timedelta
 from random import random
 import re
 
@@ -23,39 +24,69 @@ class Autoreply(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        author_id = message.author.id
-        if author_id != self.bot.user.id:
+        author = message.author
+        for name, group in self.groups.items():
 
-            for name, group in self.groups.items():
-                match = re.search(group["pattern"].format(
-                    message=message,
-                    bot=self.bot
-                ), message.content, re.IGNORECASE)
+            m = re.search(group["pattern"].format(
+                message=message,
+                bot=self.bot
+            ), message.content, re.IGNORECASE)
 
-                if match is None or random() > group["probability"]:
-                    # Negated to reduce indentation
-                    continue
-                
-                response = group["response"].format(
-                    message=message,
-                    bot=self.bot,
-                    match=match,
-                    group=match.group(),
-                    groups=match.groups(),
-                    namedgroups=match.groupdict()
-                )
+            if m is None:
+                continue
 
-                await message.channel.send(response, allowed_mentions=ALLOWED_MENTIONS)
-                self.bot.logger.debug(response)
+            conditions = True
+            for condition, value in group.get("conditions", {}).items():
+                try:
+                    match condition:
+                        case "bot":
+                            conditions &= author.bot == value
+                        case "self":
+                            conditions &= author.id == self.bot.user.id
+                        case "in guild":
+                            conditions &= message.guild.id in value
+                        case "not in guild":
+                            conditions &= message.guild.id not in value
+                        case "in channel":
+                            conditions &= message.channel.id in value
+                        case "not in channel":
+                            conditions &= message.channel.id not in value
+                        case "in users":
+                            conditions &= author.id in value
+                        case "not in users":
+                            conditions &= author.id not in value
+                        case "admin permissions":
+                            conditions &= author.guild_permissions.administrator == value
+                        case "member for":
+                            conditions &= author.joined_at >= datetime.now() + timedelta(seconds=value)
+                        case "probability":
+                            conditions &= random() <= value
+                    print(condition, conditions)
+                except Exception as e:
+                    pass
+            
+            if not conditions:
+                continue
+            
+            response = group["response"].format(
+                message=message,
+                bot=self.bot,
+                match=m,
+                group=m.group(),
+                groups=m.groups(),
+                namedgroups=m.groupdict()
+            )
 
-                if not ALLOWED_MENTIONS.everyone and message.mention_everyone:
-                    try:
-                        await message.author.edit(
-                            nick="Dummy who tried to ping everyone",
-                            reason="Tried to ping everyone"
-                        )
-                    except discord.Forbidden:
-                        pass
+            await message.reply(response, allowed_mentions=ALLOWED_MENTIONS)
 
-                if "break" in group and group["break"]:
-                    return
+            if not ALLOWED_MENTIONS.everyone and (message.mention_everyone or "@everyone" in message.content):
+                try:
+                    await message.author.edit(
+                        nick="Dummy who tried to ping everyone",
+                        reason="Tried to ping everyone"
+                    )
+                except discord.Forbidden:
+                    pass
+
+            if group.get("break", False):
+                return
